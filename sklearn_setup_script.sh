0f9d22e9870fe6c9f0252bdc893a1a2ddf36be0c
Chike Egonu <chikeegonu@gmail.com>
2025-08-19 00:00:01 +0100

    daily_commit: 2025-08-19 00:00:01

--- sklearn_setup_script.sh
+++ sklearn_setup_script.sh
@@ -1,12 +1,15 @@
 # --------------------------Scikit-learn--------------------------------
-# Clone the scikit-learn repository with all submodules
-git clone git@github.com:MrChike/scikit-learn.git
 
-# Enter the cloned repository directory
+# Clone the repo via SSH
+git clone --depth=1 git@github.com:MrChike/scikit-learn.git
+
+# Enter the repo
 cd scikit-learn
 
+# Create and switch to 'dev' branch
 git checkout -b dev
 
+# Start a Docker container for development
 docker run \
   --dns=8.8.8.8 \
   --privileged \
@@ -18,55 +21,55 @@ docker run \
   -w /sklearn_dev \
   -e PYTHONDONTWRITEBYTECODE=1 \
   -e PYTHONUNBUFFERED=1 \
-  -it python:3.10-bookworm bash
+  -it python:3.10-bookworm \
+  bash -c "$(
+    cat <<EOF
+    # Allow Git access to project dir
+    git config --global --add safe.directory /sklearn_dev
 
-cat <<EOF > setup.sh
-#!/bin/bash
+    # Install system packages
+    apt update && apt upgrade -y && apt install -y \
+        python3-pip \
+        build-essential \
+        libffi-dev \
+        libssl-dev \
+        libblas3 \
+        liblapack3 \
+        gfortran \
+        apt-utils \
+        vim \
+        net-tools \
+        procps \
+        && rm -rf /var/lib/apt/lists/*
 
-git config --global --add safe.directory /sklearn_dev
+    # Upgrade Python tools
+    pip3 install --root-user-action=ignore --upgrade pip setuptools wheel
 
-apt update && apt upgrade -y && apt install -y \\
-    python3-pip \\
-    build-essential \\
-    libffi-dev \\
-    libssl-dev \\
-    libblas3 \\
-    liblapack3 \\
-    gfortran \\
-    apt-utils \\
-    vim \\
-    net-tools \\
-    procps \\
-    && rm -rf /var/lib/apt/lists/*
+    # Install Python deps
+    pip3 install --root-user-action=ignore \
+        wheel numpy scipy cython \
+        meson-python ninja ipython \
+        notebook jupyterlab_vim jupytext
 
-# Upgrade pip and install Python build tools and dev dependencies
-pip3 install --root-user-action=ignore --upgrade pip setuptools wheel
-pip3 install --root-user-action=ignore \\
-    wheel \\
-    numpy \\
-    scipy \\
-    cython \\
-    meson-python \\
-    ninja \\
-    ipython \\
-    notebook \\
-    jupyterlab_vim \\
-    jupytext \\
+    # Inject test print into a source file
+    echo "  " >> sklearn/feature_extraction/_hash.py
+    echo '    print("Changes made by Mrchike")' >> sklearn/feature_extraction/_hash.py
 
-echo "  " >> sklearn/feature_extraction/_hash.py
-echo '    print("Changes made by Mrchike")' >> sklearn/feature_extraction/_hash.py
+    # Clean old builds
+    rm -rf build dist *.egg-info && find . -name '*.pyc' -delete && find . -name '__pycache__' -delete
 
-rm -rf build dist *.egg-info && find . -name '*.pyc' -delete && find . -name '__pycache__' -delete
+    # Install in editable mode
+    pip3 install --editable . --verbose --no-build-isolation --config-settings editable-verbose=true --root-user-action=ignore
 
-pip3 install --editable . --verbose --no-build-isolation --config-settings editable-verbose=true --root-user-action=ignore
-jupyter-lab . --allow-root --ip=0.0.0.0 --port=8888
-
+    # Start Jupyter Lab
+    jupyter-lab . --allow-root --ip=0.0.0.0 --port=8888
 EOF
+)"
 
-# You can now start working on your changes
-# NB The project has already been installed in editable mode so you don't need to rebuild upon 
-# every change but just exit from your python shell where you are executing sklearn and import it again
-# Then the changes will reflect
+# --------------------------Usage Notes----------------------------
 
-# Import the modified module to see changes
+# Code changes apply instantly (editable mode)
+# Just restart Python or re-import the module
+
+# Example test:
 # from sklearn.feature_extraction import FeatureHasher
